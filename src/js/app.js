9173ac52fbee965ea9536cfa84daaae198906ea4
Vguetz <gomezlucas712@gmail.com>
2023-10-04 22:54:43 -0300

    fix:services2

--- src/js/app.js
+++ src/js/app.js
@@ -115,7 +115,7 @@ function paginaSiguiente() {
 async function consultarAPI() {
 
     try {
-        const url = '/api/servicios';
+        const url = `${location.origin}/api/servicios`;
         const resultado = await fetch(url);
         const servicios = await resultado.json();
         mostrarServicios(servicios);
